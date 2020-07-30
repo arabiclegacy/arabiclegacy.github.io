255e876cd417639245a552fff7d2178490dc01a8
User_HP <User_HP@git.tst>
2020-07-30 17:05:05 +0200

    adding Q infographic

--- generate_mysite.tcl
+++ generate_mysite.tcl
@@ -9,6 +9,7 @@ set include_pages [list \
 	000/home.html    site_contents/00_home/content_home.html   home_list \
 	002/tajweed.html    site_contents/01_01_tajweed/tajweed.html   tajweed_list \
 	003/paradigm.html    site_contents/01_02_paradigm/paradigm.html   paradigm_list \
+	004/qinfo.html    site_contents/01_03_Qinfo/Qinfographic.html   qinfo_list \
 	001/islamic_home.html    site_contents/01_00_islamic/islamic_home.html   islamic_list \
 ]
 
@@ -47,6 +48,11 @@ set paradigm_list [list islamic_list \
  { <a href="../PATH_TO_REF" onclick="w3_close()" class="w3-bar-item w3-button w3-right ACTIVE_LIST_COLOR">
      <i class="fa fa-file-o fa-fw w3-padding-small w3-margin-left w3-right"></i>
      <div class="w3-right">النموذج المعرفي</div></a> } ]
+
+set qinfo_list [list islamic_list \
+ { <a href="../PATH_TO_REF" onclick="w3_close()" class="w3-bar-item w3-button w3-right ACTIVE_LIST_COLOR">
+     <i class="fa fa-file-o fa-fw w3-padding-small w3-margin-left w3-right"></i>
+     <div class="w3-right">Quran Infographic</div></a> } ]
 
 
 set sidebar_content ""
